f4613bca57396d309281801d9be373a312e600da
Nycrp22 <bbybianca67@gmail.com>
2025-01-05 16:19:31 -0800

    1

---
+++ resources/[Instant]/[tesla]/[car]/8Mteslacar/__resource.lua
@@ -0,0 +1,97 @@
+-- by JODKR pour 8MTesla 
+
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+
+files {
+	'data/cybertruck/handling.meta',
+	'data/cybertruck/vehicles.meta',
+	'data/cybertruck/carcols.meta',
+	'data/cybertruck/carvariations.meta',
+}
+
+files {
+	'data/model3/handling.meta',
+	'data/model3/vehicles.meta',
+	'data/model3/carcols.meta',
+	'data/model3/carvariations.meta',
+}
+
+files {
+	'data/models/handling.meta',
+	'data/models/vehicles.meta',
+	'data/models/carcols.meta',
+	'data/models/carvariations.meta',
+}
+
+files {
+	'data/modelx/handling.meta',
+	'data/modelx/vehicles.meta',
+	'data/modelx/carcols.meta',
+	'data/modelx/carvariations.meta',
+}
+
+files {
+	'data/roadster/handling.meta',
+	'data/roadster/vehicles.meta',
+	'data/roadster/carcols.meta',
+	'data/roadster/carvariations.meta',
+}
+
+files {
+	'data/semi/handling.meta',
+	'data/semi/vehicles.meta',
+	'data/semi/carcols.meta',
+	'data/semi/carvariations.meta',
+}
+
+data_file 'HANDLING_FILE' 'data/cybertruck/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/cybertruck/vehicles.meta'
+data_file 'CARCOLS_FILE' 'data/cybertruck/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/cybertruck/carvariations.meta'
+
+data_file 'HANDLING_FILE' 'data/model3/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/model3/vehicles.meta'
+data_file 'CARCOLS_FILE' 'data/model3/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/model3/carvariations.meta'
+
+data_file 'HANDLING_FILE' 'data/models/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/models/vehicles.meta'
+data_file 'CARCOLS_FILE' 'data/models/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/models/carvariations.meta'
+
+data_file 'HANDLING_FILE' 'data/modelx/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/modelx/vehicles.meta'
+data_file 'CARCOLS_FILE' 'data/modelx/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/modelx/carvariations.meta'
+
+data_file 'HANDLING_FILE' 'data/roadster/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/roadster/vehicles.meta'
+data_file 'CARCOLS_FILE' 'data/roadster/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/roadster/carvariations.meta'
+
+data_file 'HANDLING_FILE' 'data/semi/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/semi/vehicles.meta'
+data_file 'CARCOLS_FILE' 'data/semi/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/semi/carvariations.meta'
+
+-- TESLA by 8MShop&Leaks : https://discord.gg/XJBaCAtfXN 
+
+-------------------------------------------------------------------
+-------------------------------------------------------------------
+--                                                               --
+--                                                               --
+--             #######                #####         #####        -- 
+--           ###########              ######       ######        --
+--          ##         ##             ### ###     ### ###        --
+--         ##           ##            ###  ###   ###  ###        --
+--          ##         ##             ###   ### ###   ###        --
+--           ###########              ###     ###     ###        --
+--          ##         ##             ###             ###        --
+--         ##           ##            ###             ###        --
+--          ##         ##             ###             ###        --
+--           ###########              ###             ###        --
+--             #######                ###             ###        --
+--                                                               --
+--                                                               --
+-------------------------------------------------------------------
+-------------------------------------------------------------------
